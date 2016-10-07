1c17b619493cad8cca3737fa09efd9610b7fdf17
Thagus <agu.alcarin@gmail.com>
2016-10-07 10:37:07 -0500

    Abstracted friends data into Amigo class

---
+++ NodeFriends-v0.2/src/nodefriends/Amigo.java
@@ -0,0 +1,44 @@
+package nodefriends;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+/**
+ * Created by Thagus on 07/10/16.
+ */
+public class Amigo {
+    //UID Facebook, NombreUsuario, Nivel, NumeroNodo
+    String uidFacebook;
+    String nombreUsuario;
+    int nivel;
+    int numeroNodo;
+    boolean alreadyProcessed;
+
+    public Amigo(String uidFacebook, String nombreUsuario, int nivel, int numeroNodo) {
+        this.alreadyProcessed = false;
+        this.uidFacebook = uidFacebook;
+        this.nombreUsuario = nombreUsuario;
+        this.nivel = nivel;
+        this.numeroNodo = numeroNodo;
+    }
+
+    @Override
+    public int hashCode(){
+        return new HashCodeBuilder(17, 31)
+                .append(uidFacebook)
+                .toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (!(obj instanceof Amigo))
+            return false;
+        if (obj == this)
+            return true;
+
+        Amigo rhs = (Amigo) obj;
+        return new EqualsBuilder()
+                .append(uidFacebook, rhs.uidFacebook)
+                .isEquals();
+    }
+}
